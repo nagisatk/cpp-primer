c36f61767433c27980dd976013bf40afad12bfed
Nagisa <nagisa_tk@outlook.com>
2017-10-04 21:08:01 +0800

    learn

--- ch10/ex-10.3.cc
+++ ch10/ex-10.3.cc
@@ -76,10 +76,89 @@ void biggies2(vector<string> &words, vector<string>::size_type sz) {
     cout << endl;
 }
 
+// Exercise 10.20
+void biggies3(vector<string> &words, vector<string>::size_type sz) {
+    elimDups(words);
+    auto longer = [sz](const string &s) -> bool {
+        return s.size() >= sz;
+    };
+    auto count = std::count_if(words.begin(), words.end(), longer);
+    cout << count << " " << make_plural(count, "word", "s")
+         << " of length " << sz << " or longer" << endl;
+    std::for_each(words.begin(), words.end(), [sz](const string &s){ cout << (s.size() >= sz ? s + " " : ""); });
+    cout << endl;
+}
+
+// Exercise 10.21
+void exer_10_21() {
+    int a = 3;
+    auto decrease = [&a]() {
+        if (a > 0) {
+            --a;
+            return false;
+        } else 
+            return true;
+    };
+    auto test = [&a, &decrease]() {
+        cout << "a is: " << a << ", is it equal to 0: ";
+        if(decrease())
+            cout << "true";
+        else
+            cout << "false";
+        cout << endl;
+    };
+    while(a) {
+        test();
+    }
+    test();
+}
+
+// Exercise 10.22
+#include<functional>
+using namespace std::placeholders;
+bool check_size(const string &s, string::size_type sz) {
+    return s.size() >= sz;
+}
+
+void shorties(vector<string> &words, vector<string>::size_type sz) {
+    elimDups(words);
+    auto com = [](const string &s1, const string &s2) -> bool {
+        return s1.size() < s2.size();
+    };
+    std::stable_sort(words.begin(), words.end(), com);
+    auto shorter = bind(check_size, _1, sz);
+    auto wc = std::find_if(words.begin(), words.end(), shorter);
+    auto count = words.end() - wc;
+    cout << count << " " << make_plural(count, "word", "s")
+         << " of length which is shorter than " << sz << endl;
+    std::for_each(words.begin(), wc, [](const string &s){ cout << s << " "; });
+    cout << endl;
+}
+
+// Exercise 10.24
+int find_size(const string &s, const vector<int> ivec) {
+    auto f = bind(check_size, s, _1);
+    for(int i : ivec) {
+        if(!f(i))
+            return i;
+    }
+    return -1;
+}
+
 int main() {
     vector<string> words = { "the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle" };
     // elimDups(words);
     // resort_str(words);
     // lambda_sum();
-    biggies2(words, 5);
+    // biggies2(words, 5);
+    // biggies3(words, 5);
+    // exer_10_21();
+    // shorties(words, 5);
+    const string &s = "hello, world.";
+    const vector<int> ivec = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int r = find_size(s, ivec);
+    if(r == -1)
+        cout << "don't find any int bigger than the size of string: " << s << endl;
+    else
+        cout << "first int bigger than the size of string: " << s << " is " << r << endl;
 }
