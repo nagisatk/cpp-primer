19e86f849f04d1026ef32588f6351db6ff48dbed
Nagisa <nagisa_tk@outlook.com>
2017-10-13 20:14:09 +0800

    learn

--- ch12/ex-12.3.cc
+++ ch12/ex-12.3.cc
@@ -10,7 +10,57 @@ void runQuery(ifstream &infile) {
     }
 }
 
+void query_text(ifstream &infile);
 int main() {
-    ifstream ifs;
-    runQuery(ifs);
+    // Exercise 12.27
+    ifstream ifs("chapter12.txt");
+    // runQuery(ifs);
+    // Exercise 12.28
+    query_text(ifs);
+}
+
+vector<string> text;
+map<string, set<unsigned>> indexs;
+void make_result(ifstream &ifs) {
+    string line;
+    int i = 0;
+    unsigned lineno = 0;
+    while(getline(ifs, line)) {
+        text.push_back(line);
+        istringstream iss(line);
+        string word;
+        while(iss >> word) {
+            while(word[word.size()-1] == '.' || word[word.size()-1] == ',' || word[word.size()-1] == 's')
+                word = word.substr(0, word.size() - 1);
+            if(isupper(word[0]))
+                word[0] = tolower(word[0]);
+            indexs[word].insert(lineno);
+        }
+        ++lineno;
+    }
+}
+
+ostream& output_result(ostream &os, map<string, set<unsigned>>::iterator res) {
+    auto beg = res->second.begin();
+    auto end = res->second.end();
+    cout << res->first << " occurs " << res->second.size() << " time(s):" << endl;
+    while(beg != end) {
+        cout << "(line " << *beg + 1 << ") : ";
+        cout << text[*(beg++)]
+             << endl;
+    }
+    return os;
+}
+void query_text(ifstream &ifs) {
+    make_result(ifs);
+    do {
+        cout << "enter word to look for, or q for quit: ";
+        string s;
+        if(!(cin >> s) || s == "q") break;
+        auto res_pair = indexs.find(s);
+        if(res_pair != indexs.end())
+            output_result(cout, res_pair) << endl;
+        else
+            cout << s << " doesnot occur in the file." << endl;
+    } while(true);
 }
--- ch12/ex-12.3.h
+++ ch12/ex-12.3.h
@@ -5,6 +5,10 @@
 #include<fstream>
 #include<sstream>
 #include<string>
+#include<vector>
+#include<map>
+#include<set>
+#include<memory>
 
 using std::cout;
 using std::cin;
@@ -13,29 +17,95 @@ using std::ifstream;
 using std::ostream;
 using std::istringstream;
 using std::string;
+using std::vector;
+using std::map;
+using std::set;
+using std::shared_ptr;
+using std::make_shared;
 
 class QueryResult;
 class TextQuery {
 public:
-    TextQuery(ifstream& ifs): b(0) { }
+    TextQuery(ifstream& ifs);
     QueryResult query(const string& s);
+    shared_ptr<vector<string>> shared_text();
+    shared_ptr<set<unsigned int>> shared_lineno(const string&);
 private:
-    int b;
+    vector<string> text;
+    map<string, set<unsigned int>> indexs;
+    shared_ptr<vector<string>> tp = make_shared<vector<string>>(text);
 };
 
 class QueryResult {
 public:
-    QueryResult(): a(0) { }
+    QueryResult() { }
+    QueryResult(shared_ptr<vector<string>> tp, shared_ptr<set<unsigned int>> lp, string s)
+        : key_word(s), tp(tp), lp(lp), count(lp->size()) { }
+    unsigned int count_lines() {
+        return count;
+    }
+    const string& get_keyword() {
+        return key_word;
+    }
+    const string& get_line(unsigned int index) {
+        return (*tp)[index];
+    }
+    set<unsigned int>::iterator result_begin() {
+        return lp->begin();
+    }
+    set<unsigned int>::iterator result_end() {
+        return lp->end();
+    }
 private:
-    int a;
+    string key_word;
+    shared_ptr<vector<string>> tp;
+    shared_ptr<set<unsigned int>> lp;
+    unsigned int count;
 };
 
+TextQuery::TextQuery(ifstream& ifs) {
+    string line;
+    int i = 0;
+    unsigned lineno = 0;
+    while(getline(ifs, line)) {
+        text.push_back(line);
+        istringstream iss(line);
+        string word;
+        while(iss >> word) {
+            while(word[word.size()-1] == '.' || word[word.size()-1] == ',' || word[word.size()-1] == 's')
+                word = word.substr(0, word.size() - 1);
+            if(isupper(word[0]))
+                word[0] = tolower(word[0]);
+            indexs[word].insert(lineno);
+        }
+        ++lineno;
+    }
+    // cout << indexs.size() << endl;
+    // cout << text.size() << endl;
+}
+
+
+shared_ptr<vector<string>> TextQuery::shared_text() {
+    return make_shared<vector<string>>(text);
+}
+shared_ptr<set<unsigned int>> TextQuery::shared_lineno(const string& word) {
+    return make_shared<set<unsigned int>>(indexs.find(word)->second);
+}
 
 QueryResult TextQuery::query(const string& s) {
-    return QueryResult();
+    return QueryResult(shared_text(), shared_lineno(s), s);
 }
 
 ostream& print(ostream& os, QueryResult qr) {
+    auto beg = qr.result_begin();
+    auto end = qr.result_end();
+    cout << qr.get_keyword() << " occurs " << qr.count_lines() << " time(s):" << endl;
+    while(beg != end) {
+        cout << "(line " << *beg + 1 << ") : "
+             << qr.get_line(*beg)
+             << endl;
+             ++beg;
+    }
     return os;
 }
 
